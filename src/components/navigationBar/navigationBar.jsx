6b642166d4360591a023ba54cb8587dafce7b10b
Marino Gomez <marinogomez24@gmail.com>
2020-10-18 18:03:19 -0400

    Added Prettier config Added Coin like logo Added Navbar

---
+++ src/components/navigationBar/navigationBar.jsx
@@ -0,0 +1,10 @@
+import React,{Component} from 'react';
+
+
+class NavigationBar extends React.Component {
+    render() {
+      return <h1>Hello, Marino</h1>;
+    }
+  }
+
+  export default NavigationBar;
