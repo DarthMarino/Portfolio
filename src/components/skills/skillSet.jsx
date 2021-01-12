01e785fde2a00bd3affe080c4f6f83a2e965310e
Marino <marinogomez24@gmail.com>
2021-01-12 16:36:47 -0400

    Adding some animations

--- src/components/skills/skillSet.jsx
+++ src/components/skills/skillSet.jsx
@@ -1,43 +1,23 @@
+import { render } from "react-dom";
 import React, { useState } from "react";
 import { useSpring, animated } from "react-spring";
+import useMeasure from "./useMeasure";
 import "./skillSet.css";
 
 function SkillSet() {
-  const props = useSpring({
-    from: {
-      left: "0%",
-      top: "0%",
-      width: "0%",
-      height: "0%",
-      background: "lightgreen",
-    },
-    to: async (next) => {
-      while (1) {
-        await next({
-          left: "0%",
-          top: "0%",
-          width: "100%",
-          height: "100%",
-          background: "lightblue",
-        });
-        await next({ height: "50%", background: "lightgreen" });
-        await next({
-          width: "50%",
-          left: "50%",
-          background: "lightgoldenrodyellow",
-        });
-        await next({ top: "0%", height: "100%", background: "lightpink" });
-        await next({ top: "50%", height: "50%", background: "lightsalmon" });
-        await next({ width: "100%", left: "0%", background: "lightcoral" });
-        await next({ width: "50%", background: "lightseagreen" });
-        await next({ top: "0%", height: "100%", background: "lightskyblue" });
-        await next({ width: "100%", background: "lightslategrey" });
-      }
-    },
-  });
+  const [open, toggle] = useState(false);
+  const [bind, { width }] = useMeasure();
+  const props = useSpring({ width: open ? width : 0 });
   return (
-    <div className="Container">
-      <animated.div className="script-box" style={props} />
+    <div className="skill-container">
+      <div {...bind} className="main" onClick={() => toggle(!open)}>
+        <animated.div className="fill-js" style={props} />
+        <animated.div className="content">Javascript</animated.div>
+      </div>
+      <div {...bind} className="main" onClick={() => toggle(!open)}>
+        <animated.div className="fill-rb" style={props} />
+        <animated.div className="content">Ruby</animated.div>
+      </div>
     </div>
   );
 }
